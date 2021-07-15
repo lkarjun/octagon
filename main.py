ccd1a06bbfdd6114f2c6a0f3cc22e05885c49744
lkarjun <lk.arjun@hotmail.com>
2021-07-15 12:19:24 +0530

    updated descriptions

--- main.py
+++ main.py
@@ -4,8 +4,48 @@ from database import models, database
 from security import oauth2
 from routers import admin, authentication, pages, hod
 
+tags_metadata = [
+    {
+        "name": "Head Of Department",
+        "description": "Hod's unique functionalities. Only Hod can appoint new teachers, Admin can't...",
+    },
+    {
+        "name": "Admin",
+        "description": "The main role of admin is to: appoint new hod, add / delete collage course and departments...",
+    },
+    {
+        "name": "Authentication",
+        "description": "Authentication is required for everyone.",
+    },
+    {
+        "name": "Pages",
+        "description": "All webpages...",
+    },
+]
+
+description = """
+**This is my final year Project(Bca)**.
+Modified the existing 
+
+system so a teacher 
+can logon to the app using their **Face**.
+
+**Also we introduced some more unique features that is**:
+
+* hod can now set timetable without any conflits.
+* hod can send emails to other hods and teachers directly.
+                     
+"""
+
 #docs_url=None, redoc_url=None
-app = FastAPI(debug=True)
+app = FastAPI(
+            debug=True,
+            title="Teachers' Login",
+            openapi_tags=tags_metadata,
+            description=description,
+            redoc_url=None
+    )
+
 app.include_router(hod.router)
 app.include_router(authentication.router)
 app.include_router(admin.router)
