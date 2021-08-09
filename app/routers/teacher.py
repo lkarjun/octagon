fd3b9256d73e90de7b4a68ef3517bbd17815fcbf
lkarjun <lk.arjun@hotmail.com>
2021-08-09 15:44:17 +0530

    Main updated

---
+++ app/routers/teacher.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+from sqlalchemy.orm.session import Session
+from starlette.requests import Request
+from repository import hod, Schemas
+from database import database
+# from templates import TeacherTemplates
+
+router = APIRouter(tags = ['Teachers'], prefix='/teacher')
+
+get_db = database.get_db
+
+@router.get("/add-students")
+async def add_student(request: Request):
+    return "Okay"
+
+
+@router.get("/take-attendence")
+async def take_attendence(request: Request):
+    return "Attendence"
+
+
+
+
