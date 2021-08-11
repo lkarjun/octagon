a58bf97f4b5dcbdec5a978c2194543f16f67586c
lkarjun <lk.arjun@hotmail.com>
2021-08-11 22:26:35 +0530

    Files updated

---
+++ app/static/teachers_edit.js
@@ -0,0 +1,81 @@
+//Verification Image Upload
+
+function verification_image_upload(data, user_name, name){
+    var base = window.location.origin + '/hod/verification_image';
+    var form_data = new FormData()
+    form_data.append('username', user_name);
+    form_data.append('image1', $('#vimage1')[0].files[0])
+    form_data.append('image2', $('#vimage2')[0].files[0])
+    form_data.append('image3', $('#vimage3')[0].files[0])
+    console.log(form_data)
+  
+    $.ajax({
+      url: base,
+      type: 'POST',
+      async: true,
+      data: form_data,
+      cache: false,
+      
+      contentType: false,
+      processData: false,
+      success: function(result){
+        console.log("Verification Images uploaded");
+        appoint_teacher(data, name)
+      },
+      error: function(result){
+        error_alert("Something Went Wrong. Please try to contact the techincal team...");
+      }
+    })
+}
+
+// Appoint Teacher
+
+function appoint_teacher(data, name){
+    var base = window.location.origin + '/hod/Addteacher';
+    
+    $.ajax({
+      
+      url: base,
+      type: 'POST',
+      data: data,
+      dataType: 'json',
+      contentType: "application/json",
+      success: function(result){
+        $("#appoint_btn").removeClass("loading")
+      
+        swal({
+              title: "Appointed Teacher!",
+              text: "Sucessfully Appointed Teacher " + name,
+              icon: "success",
+              button: "Okay!",
+          }).then((value) => {
+                location.reload();
+                });
+
+
+      },
+      error: function(result){
+        $("#appoint_btn").removeClass("loading")
+        error_alert("Something Went Wrong. Please try to contact the techincal team...");
+      }
+     
+    })
+}
+
+$("#Appointment_form").submit((e)=>{
+    e.preventDefault()
+    $("#appoint_btn").addClass("loading")
+
+    var base = window.location.origin + '/hod/Addteacher';
+
+    var name = $("#name").val()
+    var email = $("#email").val()
+    var username = $("#username").val()
+    var phone_number = parseInt($("#number").val())
+    var tag = $("#tag").val()
+    var depart = $("#depart").val()
+    var data = JSON.stringify({"name": name, "username": username, "department": depart,
+                "email": email, "phone_number": phone_number, "tag": tag})
+    
+    verification_image_upload(data, username, name)
+  })
