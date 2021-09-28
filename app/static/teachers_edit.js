eff96572e4b220ed5037a864e2aadf7de71ef033
lalkrishna <lalkrishna@gmail.com>
2021-09-28 09:19:46 +0530

    Ui Improvement v1.0.15

--- app/static/teachers_edit.js
+++ app/static/teachers_edit.js
@@ -1,113 +1,124 @@
 //Verification Image Upload
 
 function verification_image_upload(data, user_name, name){
-    var base = window.location.origin + '/hod/verification_image';
-    var form_data = new FormData()
-    form_data.append('username', user_name);
-    form_data.append('image1', $('#vimage1')[0].files[0])
-    form_data.append('image2', $('#vimage2')[0].files[0])
-    form_data.append('image3', $('#vimage3')[0].files[0])
-    console.log(form_data)
-  
-    $("#pro").show()
-    $.ajax({
-      xhr: function() {
-        var xhr = new window.XMLHttpRequest();
-        xhr.upload.addEventListener("progress", function(evt) {
-            if (evt.lengthComputable) {
-                var percentComplete = Math.round((evt.loaded / evt.total) * 100);
-                $(".progress-bar").width(percentComplete + '%');
-                $(".progress-bar").html(percentComplete+'%');
-            }
-        }, false);
-        return xhr;
-      },
-      url: base,
-      type: 'POST',
-      async: true,
-      data: form_data,
-      cache: false,
-      
-      contentType: false,
-      processData: false,
-      beforeSend: function(){
-        $(".progress-bar").width('0%');
-        $('#uploadStatus').html('<img src="images/loading.gif"/>');
-      },
-      success: function(result){
-        console.log("Verification Images uploaded");
-        appoint_teacher(data, name)
+  var base = window.location.origin + '/hod/verification_image';
+  var form_data = new FormData()
+  form_data.append('username', user_name);
+  form_data.append('image1', $('#vimage1')[0].files[0])
+  form_data.append('image2', $('#vimage2')[0].files[0])
+  form_data.append('image3', $('#vimage3')[0].files[0])
+  console.log(form_data)
+
+  $("#pro").show()
+  $.ajax({
+    xhr: function() {
+      var xhr = new window.XMLHttpRequest();
+      xhr.upload.addEventListener("progress", function(evt) {
+          if (evt.lengthComputable) {
+              var percentComplete = Math.round((evt.loaded / evt.total) * 100);
+              $(".progress-bar").width(percentComplete + '%');
+              $(".progress-bar").html(percentComplete+'%');
+          }
+      }, false);
+      return xhr;
+    },
+    url: base,
+    type: 'POST',
+    async: true,
+    data: form_data,
+    cache: false,
+    
+    contentType: false,
+    processData: false,
+    beforeSend: function(){
+      $(".progress-bar").width('0%');
+      $('#uploadStatus').html('<img src="images/loading.gif"/>');
+    },
+    success: function(result){
+      console.log("Verification Images uploaded");
+      appoint_teacher(data, name)
+      $("#pro").hide()
+    },
+    error: function(result){
+      if(result.status == 415){
+        $("#appoint_btn").removeClass("loading disabled")
         $("#pro").hide()
-      },
-      error: function(result){
-        if(result.status == 415){
-          $("#appoint_btn").removeClass("loading disabled")
-          warning_alert("Please retake image...")
-          $("#pro").hide()
-        }
-        else{
-         error_alert("Something Went Wrong. Please try to contact the techincal team...");
-         $("#pro").hide()
-        }
-        
+        swal({
+          text: "Please retake image...",
+          icon: "warning",
+          button: "Okay"
+        })
       }
-    })
+      else{
+        $("#pro").hide()
+        swal({
+            text: "Something Went Wrong. Please try to contact the techincal team...",
+            icon: "error",
+            button: "Okay"  
+          }).then((value) => {
+            location.reload();
+          })   
+      }
+      
+    }
+  })
 }
 
 // Appoint Teacher
 
 function appoint_teacher(data, name){
-    var base = window.location.origin + '/hod/Addteacher';
+  var base = window.location.origin + '/hod/Addteacher';
+  
+  $.ajax({
     
-    $.ajax({
-      
-      url: base,
-      type: 'POST',
-      data: data,
-      dataType: 'json',
-      contentType: "application/json",
-      success: function(result){
-        $("#appoint_btn").removeClass("loading disabled")
-        $("#pro").hide()
-        swal({
-              title: "Appointed Teacher!",
-              text: "Sucessfully Appointed Teacher " + name,
-              icon: "success",
-              button: "Okay!",
+    url: base,
+    type: 'POST',
+    data: data,
+    dataType: 'json',
+    contentType: "application/json",
+    success: function(result){
+      $("#appoint_btn").removeClass("loading disabled")
+      $("#pro").hide()
+      swal({
+            title: "Appointed Teacher!",
+            text: "Sucessfully Appointed Teacher " + name,
+            icon: "success",
+            button: "Okay!",
+        })
+    },
+    error: function(result){
+      $("#pro").hide()
+      $("#appoint_btn").removeClass("loading disabled")
+      swal({
+            text: "Something Went Wrong. Please try to contact the techincal team...",
+            icon: "error",
+            button: "Okay"  
           }).then((value) => {
-                location.reload();
-                });
-
-
-      },
-      error: function(result){
-        $("#pro").hide()
-        $("#appoint_btn").removeClass("loading disabled")
-        error_alert("Something Went Wrong. Please try to contact the techincal team...");
-      }
-     
-    })
+            location.reload();
+          })
+    }
+   
+  })
 }
 
 $("#Appointment_form").submit((e)=>{
-    e.preventDefault()
-    $("#appoint_btn").addClass("loading disabled")
+  e.preventDefault()
+  $("#appoint_btn").addClass("loading disabled")
 
-    var base = window.location.origin + '/hod/Addteacher';
+  var base = window.location.origin + '/hod/Addteacher';
 
-    var name = $("#name").val()
-    var email = $("#email").val()
-    var phone_number = parseInt($("#number").val())
-    var username = $("#username").val()
-    var username = name.toLowerCase().slice(0,3) + String(phone).slice(7) + username
-    var tag = $("#tag").val()
-    var depart = $("#depart").val()
-    var data = JSON.stringify({"name": name, "username": username, "department": depart,
-                "email": email, "phone_number": phone_number, "tag": tag})
-    
-    verification_image_upload(data, username, name)
-  })
-
+  var name = $("#name").val()
+  var email = $("#email").val()
+  var phone_number = parseInt($("#number").val())
+  var username = $("#username").val()
+  var username = name.replaceAll(/\s/g,'').toLowerCase().slice(0,3) + String(phone_number).slice(7) + username
+  var tag = $("#tag").val()
+  var depart = $("#depart").val()
+  var data = JSON.stringify({"name": name, "username": username, "department": depart,
+              "email": email, "phone_number": phone_number, "tag": tag})
+  
+  verification_image_upload(data, username, name)
+})
 
   // Teacher Search
   $("#myInput").on("keyup", function() {
@@ -120,6 +131,7 @@ $("#Appointment_form").submit((e)=>{
 
   // Remove Teacher
   function remove_teacher(username, name){
+    $("#"+username).addClass("bg-danger")
     var base = window.location.origin + '/hod/Deleteteacher';
     var data = JSON.stringify({"name": name, "user_name": username});
     $.ajax({
@@ -132,10 +144,20 @@ $("#Appointment_form").submit((e)=>{
 
       success: function(result) {
         $('#'+username).remove();
-        success_alert('Successfully Removed Teacher: '+name);
+        swal({
+          text: 'Successfully Removed Teacher: '+name,
+          icon: 'success',
+          button: 'Okay'
+        });
         },
       error: function(result){
-        error_alert('Something Went Wrong Error: '+result);
+        $("#"+username).removeClass("bg-danger")
+        $("#"+username).addClass("bg-warning")
+        swal({
+          text: 'Something Went Wrong Error: '+result,
+          icon: "error",
+          button: "Okay"
+        })
         }
       });
     }
