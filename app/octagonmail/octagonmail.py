9097e94f4ba8cb2effd3c75b1ed5dd634805ba69
lalkrishna <lalkrishna@gmail.com>
2021-11-28 08:46:15 +0530

    Updates

--- app/octagonmail/octagonmail.py
+++ app/octagonmail/octagonmail.py
@@ -1,27 +1,29 @@
 import re
 import smtplib
+from email.message import EmailMessage
 import email.message
 import mimetypes
 from typing import Dict
 
-send_address = "-"
+send_address = "noreply.octagon@gmail.com"
 passs = "-"
 
-def send_email(data: Dict) -> int:
+def get_message(data: Dict) -> EmailMessage:
     """ for sending report """
-    try:
-        message = email.message.EmailMessage()
-        message["From"] = send_address
-        message["to"] = data['to']
-        message["Subject"] = data['subject']
-        message.set_content(data['body'])
-        mail_server = smtplib.SMTP_SSL('smtp.gmail.com')
+    message = EmailMessage()
+    message["From"] = send_address
+    message["to"] = data['to']
+    message["Subject"] = data['subject']
+    message.set_content(data['body'], subtype=data['subtype'])
+    return message
 
-        """ to loading some credential """
-        
+def send_mail(message: EmailMessage) -> bool:
+    """ sending mail """
+    try:
+        mail_server = smtplib.SMTP_SSL('smtp.gmail.com')
         mail_server.login(send_address, passs)
         mail_server.send_message(message)
         mail_server.quit()
-        return 0
+        return True
     except:
-        return 1
+        return False
