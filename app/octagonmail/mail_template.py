147a94e99bc100c8dcfd17606f38a8b586a78ba7
lalkrishna <lalkrishna@gmail.com>
2021-12-05 18:49:35 +0530

    mail template fixes

---
+++ app/octagonmail/mail_template.py
@@ -0,0 +1,66 @@
+html_head = '''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Octagon Service</title>
+    </head>
+    <body style="padding: 35px; margin: 10px;">
+
+    <!-- Jumbotron -->
+    {body}
+    <!-- Jumbotron -->
+
+
+    </body>
+    </html>
+'''
+
+
+verification_template = '''
+    <div style="background-color: hsl(0, 0%, 94%); padding: 30px; border-radius: 5px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+        <h2 style="font-weight: 500; text-shadow: 2px 2px 5px red;">Welcome to Octagon Family!</h2>
+        <p>
+        Hey <b>{name}</b>, we would like to inform you that we need your photos for verification.
+        </p>
+        
+        <p>
+            <a href='{link}'>Click Here </a> for verification.
+        </p>
+  
+       <hr style="border: 2px solid green; border-radius: 4px;">
+  
+
+        <p>Thanks<br>Team Octagon</p>
+    </div>
+'''
+
+
+greeting_template = '''
+
+    <div style="background-color: hsl(0, 0%, 94%); padding: 30px; border-radius: 5px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+        <h2 style="font-weight: 500; text-shadow: 2px 2px 5px red;">Hey {name}!</h2>
+        <p>
+        You've completed the verification process. Now you're part of the Octagon family. Your credentials for login:
+        </p>
+        
+        <p>
+            Username: <b>{username}</b> <br>
+            Password: <b>Face Recognition</b> <br>
+            <a href='{link}'>Click Here </a> to Login.
+            <br>
+        </p>
+
+        <p>
+        Note: Your username is the first three letters of your name + the last three digits of your number + Your Secret code. We recommend you change your secret code regularly.
+        </p>
+  
+       <hr style="border: 2px solid green; border-radius: 4px;">
+  
+
+        <p>Thanks<br>Team Octagon</p>
+    </div>
+
+'''
--- app/octagonmail/octagonmail.py
+++ app/octagonmail/octagonmail.py
@@ -4,9 +4,10 @@ from email.message import EmailMessage
 import email.message
 import mimetypes
 from typing import Dict
+from octagonmail.mail_template import *
 
 send_address = "noreply.octagon@gmail.com"
-passs = "-"
+passs = "Project@2021"
 
 def get_message(data: Dict) -> EmailMessage:
     """ for sending report """
@@ -27,3 +28,24 @@ def send_mail(message: EmailMessage) -> bool:
         return True
     except:
         return False
+
+def verification_mail(name: str, email: str, id: str):
+    id = f"http://127.0.0.1:8000/verify/{id}"
+    html = html_head.format(body=verification_template.format(name=name.upper(), link=id))
+    data = {'subject': 'Verification Needed',
+            'subtype': 'html',
+            'to': email,
+            'body': html}
+    print(send_mail(get_message(data)))
+
+
+def greeting_mail(username: str, name: str, email: str):
+    print("Greeting is sended")
+    link = f"http://127.0.0.1.8000"
+    html = html_head.format(body=greeting_template.format(name=name, username=username, email=email, link=link))
+    data = {'subject': 'Verification Completed',
+            'subtype': 'html',
+            'to': email,
+            'body': html}
+
+    print(send_mail(get_message(data)))
